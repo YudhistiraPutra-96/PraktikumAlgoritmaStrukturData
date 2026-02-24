2cfea733aea299deaf5543758fca55f027966929
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2026-02-24 07:47:30 +0700

    Modifikasi Percobaan 1

--- jobsheet02/Mahasiswa29.java
+++ jobsheet02/Mahasiswa29.java
@@ -6,6 +6,18 @@ public class Mahasiswa29 {
     String kelas;
     double ipk;
 
+    public Mahasiswa29() {
+        
+    }
+    
+    // Konstruktor berparameter
+    public Mahasiswa29(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
