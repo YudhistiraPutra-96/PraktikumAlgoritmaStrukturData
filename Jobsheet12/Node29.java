8f50037be785d8c3d43fa1b7da70cb3ff5f6b97d
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2026-05-12 09:12:53 +0700

    Praktikum

---
+++ Jobsheet12/Node29.java
@@ -0,0 +1,14 @@
+package Jobsheet12;
+
+public class Node29 {
+    Mahasiswa29 data;
+    Node29 prev;
+    Node29 next;
+
+    public Node29 (Mahasiswa29 data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+
+}
